ecce7f1b1295b89067ed4ef3375c69d11ffe782e
phastamane <phastamane@yandex.ru>
2025-05-04 23:34:37 +0300

    fetched first sity without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+const [weatherData, setWeatherData] = useState<any>(null)
+
+
+async function fetchToApi(request: string){
+  const url: string = `http://api.openweathermap.org/data/2.5/forecast?q=${request}&units=metric&lang=ru&appid=f4c00f312087165b908fcc539f5922d2`
+  const response = await fetch(url)
+  if (!response.ok){
+    throw new Error(`Ошибка: ${response.status}`)
+  }
+  
+  return await response.json()
+}
+async function onBut(){
+    
+  //  const htmlInput = (document.getElementById('task-input') as HTMLInputElement).value; 
+    try{
+      const response = await fetchToApi("Kazan")
+    setWeatherData(response)
+    }
+    catch(err){
+      console.log(err)
+    }
+
+  }
+onBut()
+
+function СityPage({ response }: { response: any }){
+  
+return(
+  <div className='city-window'>
+    <h1>{response.city.name}</h1>
+    <div className="times">
+      {[...Array(9)].map((_, i: number) =>
+      <div>
+        <h3>{response.list[i].dt_txt}</h3>
+        <p>{response.list[i].main.temp}<strong>°С</strong></p>
+        <p><strong>Облачность:</strong>{response.list[i].clouds.all}%</p>
+        
+      </div> )}
+    </div>
+  </div>
+)}
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='select-town'>
+        <h1>Какой город интересует?</h1>
+        <input id = 'task-input'/>
+        
+        <button >Найти</button>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {weatherData && <СityPage response = {weatherData} />}
     </>
   )
-}
 
+}
 export default App
